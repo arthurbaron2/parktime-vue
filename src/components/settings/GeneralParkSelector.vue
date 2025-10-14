7499027af20f71eed29a6c26da1d120cf42f3236
Arthur BARON <arthur.baron@adevinta.com>
2025-10-14 22:44:35 +0200

    add github action autodeploy

--- src/components/settings/GeneralParkSelector.vue
+++ src/components/settings/GeneralParkSelector.vue
@@ -15,7 +15,7 @@ const resorts = getResorts()
         class="text-3xl my-6 bg-slate-800 rounded-xl text-transparent bg-clip-text font-extrabold flex justify-center items-start"
         :class="parkGradientClass({ park: park?.parkId || 'default' })"
       >
-        Park Time travel
+        ParkTime
         <span
           class="text-[10px] text-white bg-gradient-to-r from-fuchsia-500 to-purple-500 py-0.5 px-1 rounded-sm ml-1 box-shadow"
         >
